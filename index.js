4e49c5b9b058e3628352219e016fb457d2c62e5e
Camila <camila_fig@hotmail.com>
2024-02-24 21:20:15 -0300

    final

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const T1 = parseFloat(prompt('Informe a temperatura inicial, em °C'))
 const T2 = parseFloat(prompt('Informe a temperatura final, em °C'))
 const L1 = parseFloat(prompt('Qual o comprimento inicial do objeto, em metros?'))
-const material = prompt('O material é feito de vidro, aço, cobre ou alumínio?')
+const material = prompt('O objeto é feito de vidro, aço, cobre ou alumínio?')
 
+console.log('Material = ', material)
 console.log('Lo = ',L1, 'metros')
 
 const variacaoTemp = (T1, T2) => {return T2-T1}
@@ -17,11 +18,10 @@ const deltaL = (T1, T2, L1, material) => {
     return L1*17* Math.pow(10,-6)*variacaoTemp(T1, T2)}
     else if (material === 'alumínio' || material === 'aluminio' || material === 'Alumínio' || material === 'Aluminio'){
     return L1*22* Math.pow(10,-6)*variacaoTemp(T1, T2)}
-    else {prompt('Você não escolheu um material inválido. Tente novamente.')}
+    else {console.log('Você não escolheu um material válido. Tente novamente.')}
 }
 
-console.log('Material = ', material)
 console.log('∆L = ',deltaL(T1, T2, L1, material), 'metros')
 
 const comprimentoFinal = (L1, deltaL) => {return L1+deltaL}
-console.log ('Comprimento final = ', L1+deltaL, 'metros')
+console.log ('Comprimento final = ', comprimentoFinal(L1, deltaL(T1, T2, L1, material)), 'metros')
